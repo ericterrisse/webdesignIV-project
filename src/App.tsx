3a9f1037354528c8211d5f092f56bf7706639fdb
Eric Terrisse <eterrissebordas@gmail.com>
2024-10-04 21:49:34 -0500

    initial config ui

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+	return (
+		<div>
+			<p className="w-[300px]">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque, facilis
+				hic reprehenderit eligendi, quae autem cupiditate consequatur
+				repudiandae commodi doloribus porro! Facilis, asperiores? Impedit quasi
+				culpa hic et a iusto.
+			</p>
+		</div>
+	);
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: ["class"],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				afacad: ['"Afacad"', "sans-serif"],
+				reemkufi: ['"Reem Kufi"', "sans-serif"],
+				montserrat: ['"Montserrat"', "sans-serif"],
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			colors: {
+				background: "hsl(var(--background))",
+				neutral1: "hsl(var(neutral-1))",
+				neutral2: "hsl(var(neutral-2))",
+				neutral3: "hsl(var(neutral-3))",
+				primary: "hsl(var(primary))",
+				secondary: "hsl(var(secondary))",
+				third: "hsl(var(third))",
+				fourth: "hsl(var(fourth))",
+				primary2: "hsl(var(primary-2))",
+				secondary2: "hsl(var(secondary-2))",
+				third2: "hsl(var(third-2))",
+				fourth2: "hsl(var(fourth-2))",
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
+};
