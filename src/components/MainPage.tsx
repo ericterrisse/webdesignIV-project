d628225a429fb73d8e346748f1c706cf771c74ab
Eric Terrisse <eterrissebordas@gmail.com>
2024-10-21 12:47:36 -0500

    respnsive main

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -7,22 +7,22 @@ const MainPage = () => {
 	return (
 		<div
 			id="main"
-			className="flex gap-8 items-center w-full justify-between h-screen"
+			className="flex gap-8 items-center w-full justify-between h-screen lg:flex-row flex-col lg:mt-0 mt-32"
 		>
 			<div className="relative">
 				<img
 					src={logo}
 					alt="Xproof logo"
-					className="h-[500px] animate-hidden-left main-imagetext"
+					className="lg:h-[500px] h-[350px] aanimate-hidden-left main-imagetext"
 				/>
-				<h1 className="font-reemkufi absolute top-[7rem] left-[18rem] text-[3.5rem] leading-relaxed text-secondary animate-hidden-left main-imagetext">
+				<h1 className="font-reemkufi absolute text-[2.5rem] top-[5rem] left-[12.5rem] lg:top-[7rem] lg:left-[18rem] lg:text-[3.5rem] leading-relaxed text-secondary aanimate-hidden-left main-imagetext">
 					is <br />
 					here <br />
 					to help
 				</h1>
 			</div>
-			<div className="flex-1 ml-16">
-				<div className="flex flex-col justify-center gap-10 animate-hidden-right">
+			<div className="flex-1 ml-0 xl:ml-16">
+				<div className="flex flex-col justify-center lg:gap-10 aanimate-hidden-right mt-10 lg:mt-0">
 					<div className="flex gap-5">
 						<h3
 							className={`text-4xl pb-2 font-semibold hover:opacity-75 hover:cursor-pointer ${
@@ -42,7 +42,7 @@ const MainPage = () => {
 						</h3>
 					</div>
 					{isStudents ? (
-						<p className="text-4xl leading-loose">
+						<p className="text-2xl lg:text-4xl leading-loose lg:leading-loose">
 							We know finding the{" "}
 							<span className="italic text-secondary ">right answer</span>{" "}
 							matters, but we believe{" "}
@@ -52,7 +52,7 @@ const MainPage = () => {
 							is what truly counts
 						</p>
 					) : (
-						<p className="text-4xl leading-loose">
+						<p className="text-2xl lg:text-4xl leading-loose lg:leading-loose">
 							We believe professors should focus their{" "}
 							<span className="italic text-secondary font-medium">
 								energy on teaching
